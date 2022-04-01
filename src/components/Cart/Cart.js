a0dd061d69e5153942a66bde368dbf737b2266d6
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-01 11:31:02 +0600

    Options - 1 : Conditional Rendering using element variable

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,6 +2,22 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
+    // Conditional rendering options
+    // 1. Element variable
+
+    let command;
+    if(cart.length === 0){
+        command = <div>
+            <h5>Spend Money</h5>
+            <p>Buy some items</p>
+        </div>
+    }
+    else if(cart.length === 1){
+        command = <p>Please add more..</p>
+    }
+    else{
+        command = <p><small>Thansks for adding item</small></p>
+    }
     return (
         <div>
             <h2>Items Selected: {cart.length}</h2>
@@ -11,6 +27,7 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
                     <button onClick={() => handleRemoveFromCart(tShirt)} >X</button>
                 </p>)
             }
+            {command}
         </div>
     );
 };
