d4588910cb34f937499393c1a4d648bca4bc423c
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-01 09:24:08 +0600

    install react router dom and component structured

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Welcome to tShirt Mania</h2>
+            <nav>
+            
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
