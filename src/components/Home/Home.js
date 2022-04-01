c3320f0625962a4a916b4ef04a45774ca88bd8ed
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-01 10:00:07 +0600

    Data display on ui

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useTshirts from '../../hooks/useTshirts';
 import Cart from '../Cart/Cart';
+import TShirt from '../TShirt/TShirt';
 import './Home.css';
 
 const Home = () => {
+    const [tShirts, setTShirts] = useTshirts();
     return (
         <div className='home-container'>
             <div className="tshirt-container">
-                
+                {
+                    tShirts.map(tShirt => <TShirt
+                        key={tShirt._id}
+                        tShirt={tShirt}
+                    ></TShirt>)
+                }
             </div>
             <div className="cart-container">
                 <Cart></Cart>
