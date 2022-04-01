5048308e193383076f56ce01e9ea444ffc536a47
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-01 13:10:30 +0600

    Prop Drilling to pass data to a many level components

--- src/components/Grandpa/Grandpa.js
+++ src/components/Grandpa/Grandpa.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Uncle from '../Uncle/Uncle';
 import Father from '../Father/Father';
 import Aunty from '../Aunty/Aunty';
 import './Grandpa.css';
 
 const Grandpa = () => {
-    const house = 7;
+    const [house, setHouse] = useState(1);
+
+    const ornament = 'Diamond Ring';
+
+    const handleBuyAHouse = () => {
+        const newHouseCount = house + 1;
+        setHouse(newHouseCount);
+    }
     return (
         <div className='grandpa'>
             <h2>Grandpa</h2>
-            <section style={{display: 'flex'}}>
-                <Father house={house}></Father>
+            <p>House: {house} <button onClick={handleBuyAHouse}>Buy a House</button></p>
+            <section style={{ display: 'flex' }}>
+                <Father house={house} ornament={ornament}></Father>
                 <Uncle house={house}></Uncle>
                 <Aunty house={house}></Aunty>
             </section>
